00938d88401fda5916f1f9675e81b54a3eed3ea2
Evgeny-Tokarev <ghenya.t@gmail.com>
2020-12-24 21:47:47 +0300

    swiper added

---
+++ src/js/main.js
@@ -0,0 +1,12 @@
+import Swiper from "swiper/bundle";
+import "swiper/swiper-bundle.css";
+import "../css/style.scss";
+var swiper = new Swiper(".swiper-container", {
+	grabCursor: true,
+	centeredSlides: true,
+	slidesPerView: "auto",
+	pagination: {
+		el: ".swiper-pagination",
+		clickable: true,
+	},
+});
